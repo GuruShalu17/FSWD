347bc976c6ba9ef9a218ac31a68c11a794c97f2b
Sivaguru90 <sivaguru90.saratha@gmail.com>
2021-08-16 10:12:46 +0800

    Bootstrap

---
+++ frontend/languages/js/intro/Ecma6-12/Modules/Static-Module/guru-bday-party.js
@@ -0,0 +1,15 @@
+
+import name from  './karshan-present.js'
+import name2 from "./karthik-present.js"
+import name3 from "./shalini-present.js"
+
+import { gift1,gift2 } from "./karshan-present.js"
+import { gift3,gift4 } from "./karthik-present.js"
+import { gift5,gift6 } from "./shalini-present.js"
+
+console.log(name)
+console.log(gift1,gift2)
+console.log(name2)
+console.log(gift3,gift4)
+console.log(name3)
+console.log(gift5,gift6())
---
+++ frontend/languages/js/intro/Ecma6-12/Modules/Static-Module/shalini-present.js
@@ -0,0 +1,8 @@
+var name3 = "shalini"
+var gift5 = {car:"honda HRV",bike:"Duke KTM"}
+var gift6 = function dating(){console.log("dating")}
+
+// each file can contain only one default export not more than that
+
+export default name3;
+export {gift5,gift6};
