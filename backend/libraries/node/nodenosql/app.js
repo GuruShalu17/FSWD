ebbfbffc74270287cb9b4a96ddbde1558f261ce5
Sivaguru90 <sivaguru90.saratha@gmail.com>
2021-09-20 23:00:43 +0800

    nodenosql

--- backend/libraries/node/nodenosql/app.js
+++ backend/libraries/node/nodenosql/app.js
@@ -3,14 +3,34 @@ require('./config/db.config')
 const Futsal = require('./src/models/players.schema')
 
 
-// const gurushalu = new Futsal({name:"Karthik",position:"Midfielder",age:23})
+const gurushalu = new Futsal({name:"Siva",position:"Defender",age:31})
 
-// gurushalu.save()
+gurushalu.save()
 
-Futsal.findOne({name:'Siva'},(err,resp)=>{
-    if(err){
-        console.log(err)
-    }else{
-        console.log(resp)
-    }
-})
+// Futsal.findOne({name:'Siva'},(err,resp)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log(resp)
+//     }
+// })
+
+// Futsal.findOneAndUpdate({name:'Karthik'},
+// {position:'Striker'},
+// {overwrite:false,new:true},
+
+// (err,resp)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log(resp)
+//     }
+// })
+
+// Futsal.findOneAndDelete({name:'Siva'},(err,resp)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log(resp)
+//     }
+// })
