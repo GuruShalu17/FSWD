d9e472e687a15f1cc03d320cad5e327c6e1dda1d
Sivaguru90 <sivaguru90.saratha@gmail.com>
2021-12-05 18:37:11 +0800

    DynamicRouting / Middleware

--- backend/frameworks/Testing/app.js
+++ backend/frameworks/Testing/app.js
@@ -7,6 +7,15 @@ app.set('port',process.env.PORT || 3000)
 let router = express.Router()
 
 
+const checkurl=(req,res,next)=>{
+    console.log("Im the Middle Ware "+req.originalUrl)
+    next()
+}
+
+app.use(checkurl)
+
+
+
 router.route('/')
 .get((req, res) => {
 
@@ -24,6 +33,42 @@ router.route('/')
 
     res.send("get api it will work only on browser")
 
+})
+
+
+app.route('/ammas/house/')
+
+
+.get((req,res)=>{
+    res.send("listen to amma advise")
+})
+.put((req,res)=>{
+    res.send("listen to amma advise")
+})
+.delete((req,res)=>{
+    res.send("listen to amma advise")
+})
+.post((req,res)=>{
+    res.send("listen to amma advise")
+})
+
+
+
+app.get("/amma/:id/:name2",(req,res)=>{
+    res.send("vanakam "+req.params.name+" "+req.params.name2)
+})
+
+
+app.get("/g/:s",(req,res)=>{
+    res.send("because we getting married ")
+})
+
+app.get('*',(req,res)=>{
+var obj={
+    statuscode:404,
+    message:"loco put correct url"
+}
+res.send(obj)
 })
 
 
